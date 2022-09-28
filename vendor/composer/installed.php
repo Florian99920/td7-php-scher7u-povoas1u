a6f548ec8e25c1e7ae745abc6d39711e46c34caa
POVOAS Florian <florian_povoas@icloud.com>
2022-09-28 10:50:54 +0200

    Ajout composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 's3/spotibuz',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '717bd0cec8286a56a8784f98f84b901f9661d1ea',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        's3/spotibuz' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '717bd0cec8286a56a8784f98f84b901f9661d1ea',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
